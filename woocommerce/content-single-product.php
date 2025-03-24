f5c99453bcd29f1c81f19c44eb7af3e15aa1c5d9
Ambre-guia <ambreguia77@gmail.com>
2025-03-24 16:51:59 +0100

    add sidebar

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -59,6 +59,7 @@ if (post_password_required()) {
 		 * @hooked WC_Structured_Data::generate_product_data() - 60
 		 */
 		do_action('woocommerce_single_product_summary');
+		dynamic_sidebar('shop_sidebar');
 		?>
 	</div>
 
