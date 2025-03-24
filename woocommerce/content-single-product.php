f8e4e7fb18288911f5cdb36f7822824c1fb6bab6
Ambre-guia <ambreguia77@gmail.com>
2025-03-24 16:33:31 +0100

    Update content-single-product.php

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The template for displaying product content in the single-product.php template
  *
@@ -16,7 +15,7 @@
  * @version 3.6.0
  */
 
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 global $product;
 
@@ -25,14 +24,14 @@ global $product;
  *
  * @hooked woocommerce_output_all_notices - 10
  */
-do_action('woocommerce_before_single_product');
+do_action( 'woocommerce_before_single_product' );
 
-if (post_password_required()) {
+if ( post_password_required() ) {
 	echo get_the_password_form(); // WPCS: XSS ok.
 	return;
 }
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class('', $product); ?>>
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
 	<?php
 	/**
@@ -41,7 +40,7 @@ if (post_password_required()) {
 	 * @hooked woocommerce_show_product_sale_flash - 10
 	 * @hooked woocommerce_show_product_images - 20
 	 */
-	do_action('woocommerce_before_single_product_summary');
+	do_action( 'woocommerce_before_single_product_summary' );
 	?>
 
 	<div class="summary entry-summary">
@@ -58,7 +57,11 @@ if (post_password_required()) {
 		 * @hooked woocommerce_template_single_sharing - 50
 		 * @hooked WC_Structured_Data::generate_product_data() - 60
 		 */
+<<<<<<< HEAD
 		do_action('woocommerce_single_product_summary');
+=======
+		do_action( 'woocommerce_single_product_summary' );
+>>>>>>> parent of 6ab9a88 (add sidebar woocommerce)
 		?>
 	</div>
 
@@ -70,8 +73,12 @@ if (post_password_required()) {
 	 * @hooked woocommerce_upsell_display - 15
 	 * @hooked woocommerce_output_related_products - 20
 	 */
-	do_action('woocommerce_after_single_product_summary');
+	do_action( 'woocommerce_after_single_product_summary' );
 	?>
 </div>
 
+<<<<<<< HEAD
 <?php do_action('woocommerce_after_single_product');
+=======
+<?php do_action( 'woocommerce_after_single_product' ); ?>
+>>>>>>> parent of 6ab9a88 (add sidebar woocommerce)
